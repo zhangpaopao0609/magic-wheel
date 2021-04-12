3489d368c9e6c798e31f35dd2d87193b94f1baa1
bo.zhang <bo.zhang@aispeech.com>
2021-04-12 13:22:45 +0800

    feat: add test

--- template-engine/__test__/index.spec.js
+++ template-engine/__test__/index.spec.js
@@ -1,16 +1,16 @@
-it("{{}} 表达式", () => {
-  const output = compile("<b>{{ name }}</b>")({ name: "tom" });
-  expect(output).toBe(`<b>tom</b>`);
+const compiler = require('../index');
+
+it("解析 {{  }}", () => {
+  const output = compiler("<h1>{{ name }}</h1>")({ name: "arrow" });
+  expect(output).toBe(`<h1>arrow</h1>`);
 });
 
 it("{{}} toUpperCase 表达式", () => {
-  const output = compile("<b>{{ name.toUpperCase() }}</b>")({ name: "tom" });
-  expect(output).toBe(`<b>TOM</b>`);
+  const output = compiler("<h1>{{ name.toUpperCase() }}</h1>")({ name: "arrow" });
+  expect(output).toBe(`<h1>ARROW</h1>`);
 });
 
-it("{{}} +连接", () => {
-  const output = compile("<b>{{ '[' + name + ']' }}</b>")({ name: "tom" });
-  expect(output).toBe(`<b>[tom]</b>`);
+it("{% %} js  语句", () => {
+  const output = compiler("{% arr.forEach(item => { %}<div>{{ item }}</div>{% }) %}")({ arr: [1, 2] });
+  expect(output).toBe(`<div>1</div><div>2</div>`);
 });
-
-
--- template-engine/index.js
+++ template-engine/index.js
@@ -1,30 +1,61 @@
-let templateStr = "{% if(isShow) { %} <b>{{ name }}</b> {% }else{ %} isShow is false {% } %} {% arr.forEach(item => { %} <div>{{ item }}</div> {% }) %}"
+// let templateStr = "{% if(isShow) { %} <b>{{ name }}</b> {% }else{ %} isShow is false {% } %} {% arr.forEach(item => { %} <div>{{ item }}</div> {% }) %}"
 
 
-// 解析 {{  }}  对应的正则
-const reMustache = /\{\{([^}]+)\}\}/g;
-// 将 {{ arg }} 直接替换成 ${ arg }
-templateStr = templateStr.replace(reMustache,  ($0, $1) => {
-  return "${" + $1.trim() + "}";
-});
+// // 解析 {{  }}  对应的正则
+// const reMustache = /\{\{([^}]+)\}\}/g;
+// // 将 {{ arg }} 直接替换成 ${ arg }
+// templateStr = templateStr.replace(reMustache,  ($0, $1) => {
+//   return "${" + $1.trim() + "}";
+// });
 
-// 解析 {% %} 对应的正则
-const reJsScript = /\{\%([^%]+)\%\}/g;
-// 首先直接获取到对应的js语句
-// 然后因为 js 不可以被包含在模板字符串中，因此需要特殊处理，前添加一个 `, 承接上一个 `， 后利用 str+= ` 来承接下面的字符串
-templateStr = templateStr.replace(reJsScript, ($0, $1) => {
-  return "`\n"+$1.trim()+"\nstr+=`";
-});
+// // 解析 {% %} 对应的正则
+// const reJsScript = /\{\%([^%]+)\%\}/g;
+// // 首先直接获取到对应的js语句
+// // 然后因为 js 不可以被包含在模板字符串中，因此需要特殊处理，前添加一个 `, 承接上一个 `， 后利用 str+= ` 来承接下面的字符串
+// templateStr = templateStr.replace(reJsScript, ($0, $1) => {
+//   return "`\n"+$1.trim()+"\nstr+=`";
+// });
 
-const head = "let str = ''\nwith(obj){\nstr += `";
-const tail = "`\n}\nreturn str;";
-const generatorStr = head + templateStr + tail;
-console.log(generatorStr);
-const generator =  new Function("obj", generatorStr);
+// const head = "let str = ''\nwith(obj){\nstr += `";
+// const tail = "`\n}\nreturn str;";
+// const generatorStr = head + templateStr + tail;
+// console.log(generatorStr);
+// const generator =  new Function("obj", generatorStr);
 
-const res1 = generator({isShow: true, name: 'arrow', arr: [1, 2]});
-console.log(res1);
-// <b>arrow</b>   <div>1</div>  <div>2</div>
-const res2 = generator({isShow: false, name: 'arrow', arr: [3, 4]});
-console.log(res2);
-// isShow is false   <div>3</div>  <div>4</div>
+// const res1 = generator({isShow: true, name: 'arrow', arr: [1, 2]});
+// console.log(res1);
+// // <b>arrow</b>   <div>1</div>  <div>2</div>
+// const res2 = generator({isShow: false, name: 'arrow', arr: [3, 4]});
+// console.log(res2);
+// // isShow is false   <div>3</div>  <div>4</div>
+
+
+
+
+
+
+
+function compiler(templateStr) {
+  // 解析 {{  }}  对应的正则
+  const reMustache = /\{\{([^}]+)\}\}/g;
+  // 将 {{ arg }} 直接替换成 ${ arg }
+  templateStr = templateStr.replace(reMustache,  ($0, $1) => {
+    return "${" + $1.trim() + "}";
+  });
+
+  // 解析 {% %} 对应的正则
+  const reJsScript = /\{\%([^%]+)\%\}/g;
+  // 首先直接获取到对应的js语句
+  // 然后因为 js 不可以被包含在模板字符串中，因此需要特殊处理，前添加一个 `, 承接上一个 `， 后利用 str+= ` 来承接下面的字符串
+  templateStr = templateStr.replace(reJsScript, ($0, $1) => {
+    return "`\n"+$1.trim()+"\nstr+=`";
+  });
+
+  const head = "let str = ''\nwith(obj){\nstr += `";
+  const tail = "`\n}\nreturn str;";
+  const generatorStr = head + templateStr + tail;
+  const generator =  new Function("obj", generatorStr);
+  return obj => generator(obj);
+};
+
+module.exports = compiler;
