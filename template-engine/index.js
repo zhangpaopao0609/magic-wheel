3794723d82fec0c5d91df857cbdbc3fc53776cf4
paopao-zhang <ardor-zhang@outlook.com>
2021-09-25 13:28:01 +0800

    feat: add template-engine ppt

--- template-engine/index.js
+++ template-engine/index.js
@@ -39,7 +39,7 @@ function compiler(templateStr) {
   // 解析 {{  }}  对应的正则
   const reMustache = /\{\{([^}]+)\}\}/g;
   // 将 {{ arg }} 直接替换成 ${ arg }
-  templateStr = templateStr.replace(reMustache,  ($0, $1) => {
+  let templateRenderRes = templateStr.replace(reMustache,  ($0, $1) => {
     return "${" + $1.trim() + "}";
   });
 
@@ -47,13 +47,13 @@ function compiler(templateStr) {
   const reJsScript = /\{\%([^%]+)\%\}/g;
   // 首先直接获取到对应的js语句
   // 然后因为 js 不可以被包含在模板字符串中，因此需要特殊处理，前添加一个 `, 承接上一个 `， 后利用 str+= ` 来承接下面的字符串
-  templateStr = templateStr.replace(reJsScript, ($0, $1) => {
-    return "`\n"+$1.trim()+"\nstr+=`";
+  templateRenderRes = templateRenderRes.replace(reJsScript, ($0, $1) => {
+    return "`\n"+$1.trim()+";\nstr+=`";
   });
 
-  const head = "let str = ''\nwith(obj){\nstr += `";
-  const tail = "`\n}\nreturn str;";
-  const generatorStr = head + templateStr + tail;
+  const head = "let str = '';\nwith(obj){\nstr += `";
+  const tail = "`\n};\nreturn str;";
+  const generatorStr = head + templateRenderRes + tail;
   const generator =  new Function("obj", generatorStr);
   return obj => generator(obj);
 };
