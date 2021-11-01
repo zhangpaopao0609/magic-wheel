e1b9cea78705bcef2fd87a53bdf77bc417f6835c
paopao-zhang <ardor-zhang@outlook.com>
2021-11-01 16:46:18 +0800

    feat: change throttle

---
+++ 02-debounce-throttle/src/throttle/02-throttle-v2.js
@@ -0,0 +1,20 @@
+/**
+ * 节流函数
+ * @param {*} fn 需要执行的事件（操作）
+ * @param {*} interval 每隔 interval 这段时间都需要执行一次
+ */
+function throttle(fn, interval) {
+  let time = Date.now();
+
+  return function(...args) {
+    const i = Date.now();
+    if (i - time < interval) {
+      return;
+    } else {
+      setTimeout(() => {
+        fn(...args);
+      }, interval);
+      time = i;
+    };
+  }
+}
